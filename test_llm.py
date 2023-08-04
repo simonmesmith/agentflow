3b70769733a5124d491e6b1dd2b0d9616914ecbc
Simon Smith <simon@benchsci.com>
2023-08-04 16:23:17 -0400

    Fix bug

--- test_llm.py
+++ test_llm.py
@@ -1,15 +1,20 @@
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
-from llm import LLM, Settings
+from llm import Settings
 
 
-@patch.object(
-    LLM, "respond", return_value={"role": "assistant", "content": "Yes, I am here!"}
-)
-def test_respond(mock_respond):
-    llm = LLM()
+@patch("llm.LLM")
+def test_respond(mock_llm_class):
+    mock_llm_instance = MagicMock()
+    mock_llm_instance.respond.return_value = {
+        "role": "assistant",
+        "content": "Yes, I am here!",
+    }
+    mock_llm_class.return_value = mock_llm_instance
+
+    llm = mock_llm_class()
     settings = Settings()
     messages = [{"role": "user", "content": "This is a test. Are you there?"}]
     response = llm.respond(settings, messages)
     assert response is not None
-    mock_respond.assert_called_once_with(settings, messages)
+    mock_llm_instance.respond.assert_called_once_with(settings, messages)
