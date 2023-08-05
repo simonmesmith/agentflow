90af5c68a7319cec4a4beb5ffc40595193427ae2
Simon Smith <simon@benchsci.com>
2023-08-05 15:32:30 -0400

    Fix bug in CI for #13

--- agentflow/functions/test_create_image.py
+++ agentflow/functions/test_create_image.py
@@ -1,14 +1,20 @@
 import os
 import re
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 from agentflow.functions.create_image import CreateImage
 from agentflow.output import Output
 
 
 @patch("openai.Image.create")
-def test_execute(mock_create):
+@patch("requests.get")
+def test_execute(mock_get, mock_create):
     mock_create.return_value = {"data": [{"url": "https://mockurl.com/mock_image.jpg"}]}
+
+    # Mock the requests.get call to return a mock response with mock image content
+    mock_response = MagicMock()
+    mock_response.content = b"mock image content"
+    mock_get.return_value = mock_response
 
     output = Output("test_create_image_execute")
     create_image = CreateImage(output)
@@ -16,6 +22,10 @@ def test_execute(mock_create):
 
     # Check that the returned image name is a valid SHA-256 hash followed by ".png"
     assert re.match(r"[0-9a-f]{64}\.png$", result) is not None
+
+    # Check that the image file was created with the correct content
+    with open(f"{output.output_path}/{result}", "rb") as f:
+        assert f.read() == b"mock image content"
 
     os.remove(f"{output.output_path}/{result}")
     os.rmdir(output.output_path)
