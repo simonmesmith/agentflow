8b7da64d35d30e84ff87cbc0206a29d4bf86e7d4
Simon Smith <simon@benchsci.com>
2023-08-06 15:06:38 -0400

    Improve flow testing to close #29

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -2,6 +2,7 @@
 This module contains tests for the Flow class.
 """
 
+import json
 import os
 from types import SimpleNamespace
 from typing import Dict, List, Optional
@@ -19,31 +20,31 @@ def mock_llm_respond(
     functions: Optional[List[Dict[str, str]]] = None,
 ) -> Dict[str, str]:
     """
-    This function is used as a dynamic response function for testing purposes.
-    :param settings: The settings for the interaction.
-    :type settings: Settings
-    :param messages: The messages to be processed by the mocked language model.
-    :type messages: List[Dict[str, str]]
-    :param functions: The functions to be processed by the mocked language model.
-    :type functions: Optional[List[Dict[str, str]]]
+    Mock the LLM respond method.
     """
-    last_message_content = messages[-1]["content"]
     return SimpleNamespace(
-        role="assistant", content=f"Response to {last_message_content}"
+        role="assistant", content=f"Response to {messages[-1]['content']}"
     )
+
+
+def mock_function_definition(function_name: str) -> dict:
+    """
+    Mock a function definition.
+    """
+    return {"name": f"{function_name} definition"}
 
 
 @pytest.fixture
 def flows_path():
     """
-    Fixture that returns the path to the test flows folder.
+    Get the path to the test flows directory.
     """
     return os.path.dirname(os.path.abspath(__file__))
 
 
 def test_file_not_found(flows_path):
     """
-    Tests the initialization of the Flow object with a nonexistent flow file.
+    Test that a FileNotFoundError is raised if the flow file does not exist.
     """
     with pytest.raises(FileNotFoundError):
         _ = Flow("file_not_found", flows_path=flows_path)
@@ -51,150 +52,93 @@ def test_file_not_found(flows_path):
 
 def test_flow_basic(flows_path):
     """
-    Tests the initialization and running of a Flow object with a basic flow file.
+    Test that we can load and run a basic flow.
     """
-
-    # Load the flow
-    flow = Flow("test_flow_basic", flows_path=flows_path)
-
-    # Ensure we're loading the system message and all tasks
-    assert flow.system_message == "Test system message.", "System message is incorrect."
-    assert len(flow.tasks) == 3, "Number of tasks is incorrect."
-
-    # Ensure we're loading the correct settings for each task
-    default_settings = Settings()
-    assert flow.tasks[0].settings == default_settings, "Settings are incorrect."
-
-    # Test running the flow
     with patch("agentflow.flow.LLM") as MockLLM:
         mock_llm = MockLLM.return_value
         mock_llm.respond.side_effect = mock_llm_respond
+
+        flow = Flow("test_flow_basic", flows_path=flows_path)
+
+        assert flow.system_message == "Test system message."
+        assert len(flow.tasks) == 3
+
+        # Test that settings get loaded correctly
+        with open(os.path.join(flows_path, "test_flow_basic.json"), "r") as file:
+            flow_json = json.load(file)
+            flow_json_test_settings = flow_json["tasks"][0]["settings"]
+            flow_object_settings = [
+                s for s in flow.tasks[0].settings.__dict__ if s != "function_call"
+            ]
+            for setting in flow_object_settings:
+                assert (
+                    getattr(flow.tasks[0].settings, setting)
+                    == flow_json_test_settings[setting]
+                )
+
+        # Test that we use default settings if there are none provided
+        assert flow.tasks[1].settings == Settings()
+
         flow.run()
 
-        # Ensure we're poperly instantiating the LLM and calling the respond method
-        assert MockLLM.call_count == 1, "LLM was not instantiated exactly once."
-        assert mock_llm.respond.call_count > 0, "The respond method was not called."
+        assert MockLLM.call_count == 1
+        assert mock_llm.respond.call_count > 0
 
-        # Ensure we're passing the correct messages to the LLM
         last_call = mock_llm.respond.call_args
         last_messages = last_call[0][1]
         assert (
             last_messages[-1]["content"]
             == f"Response to {last_messages[-2]['content']}"
-        ), "The LLM respond method is not working as intended."
+        )
+
+        os.remove(os.path.join(flow.output.output_path, "messages.json"))
+        os.rmdir(flow.output.output_path)
 
 
-# def test_flow_no_variables(flow_path):
-#     """
-#     Tests the initialization of the Flow object with no variables.
-#     """
-#     data = {
-#         "system_message": "Test system message.",
-#         "tasks": [
-#             {"action": "Test action 1"},
-#             {"action": "Test action 2"},
-#         ],
-#     }
+def test_flow_with_variables(flows_path):
+    variables = {
+        "system_message_variable": "system_message_variable_value",
+        "task_1_variable": "task_1_variable_value",
+    }
+    flow = Flow("test_flow_with_variables", variables, flows_path)
 
-#     with open(flow_path, "w") as f:
-#         json.dump(data, f)
+    # Test that we set variables correctly
+    assert flow.system_message == "System message with system_message_variable_value."
+    assert flow.tasks[0].action == "Task 1 action with task_1_variable_value."
+    assert (
+        flow.tasks[1].action
+        == "Task 2 action with {task_2_curly_bracket_non_variable}."
+    )
 
-#     flow = Flow("test")
-#     assert flow.system_message == data["system_message"]
-#     assert all(
-#         task.action == data["tasks"][i]["action"] for i, task in enumerate(flow.tasks)
-#     )
+    # Test that we raise an error if we provide extra variables
+    variables["extra_variable"] = "extra_variable_value"
+    with pytest.raises(
+        ValueError, match="Extra variables provided: {'extra_variable'}."
+    ):
+        _ = Flow("test_flow_with_variables", variables, flows_path)
 
-#     os.remove(flow_path)
+    # Test that we raise an error if we don't provide all variables
+    variables.pop("extra_variable")
+    variables.pop("system_message_variable")
+    with pytest.raises(
+        ValueError, match="Missing variable values for: {'system_message_variable'}."
+    ):
+        _ = Flow("test_flow_with_variables", variables, flows_path)
 
-
-# def test_flow_with_variables_correct_values(flow_path):
-#     """
-#     Tests the initialization of the Flow object with correct variable values.
-#     """
-#     data = {
-#         "system_message": "Test system message with {variable}.",
-#         "tasks": [
-#             {"action": "Test action 1 with {variable}"},
-#             {"action": "Test action 2"},
-#         ],
-#     }
-
-#     with open(flow_path, "w") as f:
-#         json.dump(data, f)
-
-#     flow = Flow("test", {"variable": "value"})
-#     assert flow.system_message == data["system_message"].format(variable="value")
-#     assert flow.tasks[0].action == data["tasks"][0]["action"].format(variable="value")
-#     assert flow.tasks[1].action == data["tasks"][1]["action"]
-
-#     os.remove(flow_path)
+    os.rmdir(flow.output.output_path)
 
 
-# def test_flow_with_variables_extra_values(flow_path):
-#     """
-#     Tests the initialization of the Flow object with extra variable values.
-#     """
-#     data = {
-#         "system_message": "Test system message.",
-#         "tasks": [
-#             {"action": "Test action 1"},
-#             {"action": "Test action 2"},
-#         ],
-#     }
+def test_flow_with_functions(flows_path):
+    with patch("agentflow.flow.Function") as MockFunction:
+        with patch("agentflow.flow.LLM") as MockLLM:
+            mock_function = MockFunction.return_value
+            mock_function.definition.side_effect = mock_function_definition
 
-#     with open(flow_path, "w") as f:
-#         json.dump(data, f)
+            mock_llm = MockLLM.return_value
+            mock_llm.respond.side_effect = mock_llm_respond
 
-#     with pytest.raises(ValueError, match="Extra variables provided"):
-#         _ = Flow("test", {"variable": "value"})
+            flow = Flow("test_flow_with_functions", flows_path=flows_path)
 
-#     os.remove(flow_path)
+            assert len(flow.functions) == 2
 
-
-# def test_flow_with_variables_missing_values(flow_path):
-#     """
-#     Tests the initialization of the Flow object with missing variable values.
-#     """
-#     data = {
-#         "system_message": "Test system message with {variable}.",
-#         "tasks": [
-#             {"action": "Test action 1 with {variable}"},
-#             {"action": "Test action 2"},
-#         ],
-#     }
-
-#     with open(flow_path, "w") as f:
-#         json.dump(data, f)
-
-#     with pytest.raises(ValueError, match="Missing variable values for"):
-#         _ = Flow("test")
-
-#     os.remove(flow_path)
-
-
-# def test_flow_with_escaped_curly_braces(flow_path):
-#     """
-#     Tests the initialization of the Flow object with escaped curly braces in the system message and tasks.
-#     """
-#     data = {
-#         "system_message": "Test system message with {{escaped}} braces.",
-#         "tasks": [
-#             {"action": "Test action 1 with {{escaped}} braces"},
-#             {"action": "Test action 2"},
-#         ],
-#     }
-
-#     with open(flow_path, "w") as f:
-#         json.dump(data, f)
-
-#     flow = Flow("test")
-#     assert flow.system_message == data["system_message"].replace("{{", "{").replace(
-#         "}}", "}"
-#     )
-#     assert flow.tasks[0].action == data["tasks"][0]["action"].replace(
-#         "{{", "{"
-#     ).replace("}}", "}")
-#     assert flow.tasks[1].action == data["tasks"][1]["action"]
-
-#     os.remove(flow_path)
+            os.rmdir(flow.output.output_path)
